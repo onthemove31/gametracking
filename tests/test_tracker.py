389a21ce7c850868a2036e4049f13da536faca4f
onthemove31 <kamall@outlook.com>
2024-09-01 17:21:59 +0530

    Refactor

--- tests/test_tracker.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from modules.tracker import track_game_sessions
-
-class TestGameSessionTracker(unittest.TestCase):
-    # Mock tests to be implemented as per the specific requirements
-    pass
-
-if __name__ == '__main__':
-    unittest.main()
