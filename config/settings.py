629950e8cbe4fcb8236fb1cc6653fddae1679ef5
onthemove31 <kamall@outlook.com>
2024-09-01 17:21:59 +0530

    Refactor

--- config/settings.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-def load_config():
-    load_dotenv()
-    return {
-        'DB_PATH': os.getenv('DB_PATH', 'game_sessions.db')
-    }
--- utils/logger.py
+++
@@ -1,5 +0,0 @@
-import logging
-
-def setup_logging():
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', filename='game_session_tracker.log', filemode='a')
-    logging.info("Logging setup complete.")
